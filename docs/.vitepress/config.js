97569318579c4a686d67bec5e5a9da5d3e55d9af
Graysonnwu <185429663@qq.com>
2022-10-08 15:04:42 +0800

    美化1-122期开头并添加谷歌搜索所有权认证

--- docs/.vitepress/config.js
+++ docs/.vitepress/config.js
@@ -14,6 +14,7 @@ export default withMermaid(
       ['link', { rel: 'icon', type: 'image/png', sizes: '32x32', href: faviconHref }],
       ['link', { rel: 'icon', type: 'image/png', sizes: '16x16', href: faviconHref }],
       ['link', { rel: 'shortcut icon', href: faviconHref }],
+      ['meta', {name: "google-site-verification", content: "dlHmuEjhVXOqsfWy29EX9OcngNyw4jMGSbmJ_n6oph8"}]
     ],
     themeConfig: {
       siteTitle: 'EggrollDatabase',
